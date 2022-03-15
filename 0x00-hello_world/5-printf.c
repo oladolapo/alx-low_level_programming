e50037aa424986d087deda3c38e85737709f65ba
oladolapo <dolapoogunsipe@gmail.com>
2022-03-14 18:33:21 -0700

    update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: This is a great main function
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (o);
+}
